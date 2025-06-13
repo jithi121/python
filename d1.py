424129be86de8feab527a4c1283d1067ddce3243
shields <jithi121@gmail.com>
2025-06-13 21:59:52 +0100

    again

--- d1.py
+++ d1.py
@@ -10,3 +10,38 @@ print("hello")
 # it is Case Sensitive
 # semi colon is not necessary, but good to use if we use multiple statement in one line  >  statement1; statement2; statement3
 
+
+#data types: 
+# int  t = 1
+# float t = 1.3 
+# strings t = "stringsss" or "strings"
+# booleans = t = True
+
+# Naming Variables 
+#  (a-z, A-Z), numbers (0-9) and underscores (_). They cannot start with a number.
+# name =! Name
+# 
+# Arithmetic ops
+# 
+
+# # sum = a + b   # 13
+# subtraction = a - b    # 7
+# multiplication = a * b    # 30
+# division = a / b   # 3.333333333
+# floor_division = a // b   # 3
+# modulus = a % b   # 1
+# exponentiation = a ** b   # 1000
+# 
+# 
+# #
+
+# equal = a == b   # False
+# not_equal = a != b   # True
+# greater_than = a > b   # True
+# less_than = a < b   # False
+# greater_or_equal = a >= b   # True
+# less_or_equal = a <= b   # False
+
+# AND (and): returns True if both conditions are true.
+# OR (or): returns True if at least one of the conditions is true.
+# NOT (not): inverts the value of a condition, returns True if the condition is false and False if the condition is true.
