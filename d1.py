b103466a1279570cec3b6c911e0e640267247712
shields <jithi121@gmail.com>
2025-06-13 21:59:29 +0100

    comparison

--- d1.py
+++ d1.py
@@ -7,3 +7,6 @@ print("hello")
 
 #  ## > single 
 # """ """  > multi 
+# it is Case Sensitive
+# semi colon is not necessary, but good to use if we use multiple statement in one line  >  statement1; statement2; statement3
+
