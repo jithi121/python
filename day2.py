b77fa5fc945a43c39c150d659b3b803c64582e5c
shields <jithi121@gmail.com>
2025-07-10 01:04:59 +0100

    day2 with cpt

---
+++ day2.py
@@ -0,0 +1,9 @@
+import os
+print("hello...!, share password")
+sas =  input("what is the pass:")
+print(sas)
+os.system('cls')
+
+age, name, relation_status = input("enter your age, name and relationship")
+if(age<18)
+    print("Oh no, little one. Come back when you’re legal to code.")
