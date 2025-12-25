9ac038db90d3819e60240b9e3a40e064ba0af6a1
shields <jithi121@gmail.com>
2025-12-25 19:47:59 +0000

    day_1

--- 30_days_of_python/day2/day2.py
+++ 30_days_of_python/day2/day2.py
@@ -1,1 +1,16 @@
 #Day 2: 30 Days of python programming
+
+first_name = 'Jithin'
+last_name = 'Surendran'
+full_name = first_name + ' ' +  last_name;
+print(full_name);
+country = 'India'
+city = 'Thrissur'
+age = 32
+year = 1993
+is_married = 'true'
+is_true = '0'
+is_light_on = '1'
+a,b,c = 1,2,3
+
+
